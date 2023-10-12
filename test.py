3996a5c8524d74b31d30d72c8442dd8fa9d11828
WizdShadow <enderoffice7@gmail.com>
2023-10-12 16:02:30 +0300

    GAME
    GAME

--- test.py
+++ test.py
@@ -68,18 +68,20 @@ while True:
                 xp_players2 -= 5
                 xp_players -= 5
                 spos += 10
-            elif "Доспех огня" not in gamebafdebuf2.values():
-                xp_players2 -= 10
-                spos += 15
-            elif "Плащь тьмы" in gamebafdebuf2.values():
+            elif "Плащ тьмы" in gamebafdebuf2.values():
+                print("плащьт есть")
                 chance = 0.4 # Шанс выпадения
-                if random.random() < chance:
+                if random.random() < chance:    
                     print(f"Вы нанесли урон")
-                    xp_players2 -= 10
+                    xp_players2 -= 15
                     spos += 15
                 else:
                     print(f"Вы промахнулись")
                     spos += 10
+            
+            elif "Доспех огня" not in gamebafdebuf2.values():
+                xp_players2 -= 10
+                spos += 15
             
 
         elif action == "2":
@@ -99,7 +101,59 @@ while True:
                     continue
                 elif games == "Дмитрия Теневеда":
                     print(f"Вы слились с тьмой и вероятность по вам попасть 40%")
-                    gamebafdebuf["baf"] = "Плащь тьмы"
+                    gamebafdebuf["baf"] = "Плащ тьмы"
+                    spos -= 100
+                    continue
+            else:
+                print(f"Ваша способность не готова {spos}")
+        else:
+            print(f"Такого хода нету")
+            continue
+        
+        
+    print(f"Ход первого игрока")
+    if not time_stop:
+        action = input("Выберите ход написав цифру: \n1)Атака \n2)Способность \n")
+        
+        if action == "1":
+            if "Доспех огня" in gamebafdebuf2.values():
+                xp_players -= 5
+                xp_players2 -= 5
+                spos += 10
+            elif "Плащ тьмы" in gamebafdebuf2.values():
+                print("плащьт есть")
+                chance = 0.4 # Шанс выпадения
+                if random.random() < chance:    
+                    print(f"Вы нанесли урон")
+                    xp_players -= 15
+                    spos += 15
+                else:
+                    print(f"Вы промахнулись")
+                    spos += 10
+            
+            elif "Доспех огня" not in gamebafdebuf2.values():
+                xp_players -= 10
+                spos += 15
+            
+
+        elif action == "2":
+            if spos >= 100:
+                spos = 100
+            if spos == 100:
+                if games == "Василису Огненкая":
+                    print(f"Вы наложили на себя огненый доспех")
+                    gamebafdebuf2["baf"] = "Доспех огня"
+                    spos -= 100
+                    continue
+                elif games == "Алису Мгновенную":
+                    print(f"Вы остановили время 2 игроку")
+                    gamebafdebuf["debaf2"] = "Остановка времени"
+                    time_stop2 = True
+                    spos -= 100
+                    continue
+                elif games == "Дмитрия Теневеда":
+                    print(f"Вы слились с тьмой и вероятность по вам попасть 40%")
+                    gamebafdebuf2["baf"] = "Плащ тьмы"
                     spos -= 100
                     continue
             else:
@@ -110,59 +164,9 @@ while True:
         
     
         
-    print(f"Ход Второго игрока игрока")
-    if not time_stop2:
-        action2 = random.randint(1, 2)
-        
-        if action2 == 1:
-            if "Доспех огня" in gamebafdebuf.values():
-                xp_players -= 5
-                xp_players2 -= 5
-                spos2 += 10
-            elif "Доспех огня" not in gamebafdebuf.values():
-                xp_players -= 10
-                spos2 += 15
-            elif "Плащь тьмы" in gamebafdebuf.values():
-                chance = 0.4 # Шанс выпадения
-                if random.random() < chance:
-                    print(f"Вы нанесли урон")
-                    xp_players -= 10
-                    spos2 += 15
-                else:
-                    print(f"Вы промахнулись")
-                    spos2 += 10
-            
-
-        elif action2 == 2:
-            if spos == 100:
-                if games == "Василису Огненкая":
-                    if spos2 == 100:
-                        print(f"Вы наложили на себя огненый доспех")
-                        gamebafdebuf2["baf"] = "Доспех огня"
-                        spos2 -= 100
-                        continue
-                elif games == "Алису Мгновенную":
-                    if spos2 == 100:
-                        print(f"Вы остановили время 2 игроку")
-                        gamebafdebuf["debaf2"] = "Остановка времени"
-                        time_stop = True
-                        spos2 -= 100
-                        continue
-                elif games == "Дмитрия Теневеда":
-                    if spos2 == 100:
-                        print(f"Вы слились с тьмой и вероятность по вам попасть 40%")
-                        gamebafdebuf2["baf"] = "Плащ тьмы"
-                        spos2 -= 100 
-                        continue
-            else:
-                print(f"Способность второго игрока не готова {spos2}")
-        else:
-            print(f"Такого хода нету")
-            continue
-    else:
-        print(f"Вы застыли во времени и неможете нечего сделать")  
-        
     print (f"Жизни игроков {xp_players},{xp_players2}") 
+    print (gamebafdebuf)
+    print(gamebafdebuf2)
         
         
     if xp_players <= 0:
